ee787cf253387e6541089dda7354d43bc5fe77f9
edi724 <73525131+edi724@users.noreply.github.com>
2024-08-31 18:23:03 +0300

    Update idk.js

--- idk.js
+++ idk.js
@@ -1,24 +1,8 @@
 p = document.getElementById("1")
 a = document.getElementById("idk")
-d = 0
+b = document.getElementById("idk1")
 a.addEventListener("click", () => {
-    c = 0
-    while (c == 0) {
-        b = Math.floor(Math.random() * 3)
-        if (b == 0 && d!=b) {
-            p.innerHTML = "Rock"
-            d = b
-            c = 1
-        }
-        if (b == 1 && d!=b) {
-            p.innerHTML = "Paper"
-            d = b
-            c = 1
-        }
-        if (b == 2 && d!=b) {
-            p.innerHTML = "Scissors"
-            d = b
-            c = 1
-        }
-    }
+    localStorage.setItem("idk", b.value)
+    p.innerHTML = localStorage.getItem("idk")
 })
+setInterval(() => p.innerHTML = localStorage.getItem("idk"), 1000)
