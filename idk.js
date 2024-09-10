a76b62e538098888ee676448e11cc829bd4f52e2
edi724 <73525131+edi724@users.noreply.github.com>
2024-09-10 09:36:20 +0300

    Update idk.js

--- idk.js
+++ idk.js
@@ -1,8 +1,27 @@
-p = document.getElementById("1")
 a = document.getElementById("idk")
-b = document.getElementById("idk1")
+input = document.getElementById("input")
+div = document.getElementById("container")
+let top1 = 100
+let right = 1000
+let right1 = 120
 a.addEventListener("click", () => {
-    localStorage.setItem("idk", b.value)
-    p.innerHTML = localStorage.getItem("idk")
+    let p1 = document.createElement("p")
+    let button = document.createElement("a")
+    button.innerHTML = "X"
+    button.href = "#"
+    button.style.background = "red"
+    button.style.color = "white"
+    p1.innerHTML = input.value
+    console.log(p1)
+    p1.style.top = top1 + "px"
+    p1.style.right = right + "px"
+    div.appendChild(p1)
+    p1.appendChild(button)
+    button.style.right = right1 + "px"
+    top1 += 30
+    button.addEventListener("click", () => {
+        p1.remove()
+        button.remove()
+        top1 -= 30
+    })
 })
-setInterval(() => p.innerHTML = localStorage.getItem("idk"), 1000)
