374420be06d1d2dcc7263611df1d7750d49bf715
edi724 <73525131+edi724@users.noreply.github.com>
2024-12-04 09:53:10 +0200

    Update idk.js

--- idk.js
+++ idk.js
@@ -1,7 +1,7 @@
-  const socket = new WebSocket('ws://localhost:8080');
-  const input = document.getElementById("1")
-  const p = document.getElementById("2")
-  socket.onmessage = (message) => p.innerHTML= message.data;
- function send_message(){
-    socket.send(input.value)
-  }
+const socket = new WebSocket('ws://localhost:8080');
+const input = document.getElementById("1")
+const p = document.getElementById("2")
+socket.onmessage = (message) => p.innerHTML= message.data;
+function send_message(){
+  socket.send(input.value)
+}
