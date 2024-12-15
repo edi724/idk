43629864bca30440157ddd946e74ca251902f580
edi724 <73525131+edi724@users.noreply.github.com>
2024-12-15 16:07:48 +0200

    Update client.js

--- client.js
+++ client.js
@@ -1,10 +1,15 @@
-  const socket = new WebSocket('wss://e284-2a02-2f09-3f14-fa00-2c85-1f7a-3d45-5eeb.ngrok-free.app');
+  const socket = new WebSocket('wss://eager-gazelle-plainly.ngrok-free.app');
   const input = document.getElementById("1")
-  const p = document.getElementById("2")
+  const div = document.getElementById("2")
   const input2 = document.getElementById("3")
   const b = document.getElementById("4")
   const b2 = document.getElementById("5")
-  socket.onmessage = (message) => p.innerHTML= message.data;
+  socket.onmessage = (message) => {
+    let para = document.createElement("p");
+    para.innerHTML = message.data;
+    div.appendChild(para);
+  };
+  
  function send_message(){
     socket.send(["1"+input.value])
   }
